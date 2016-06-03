1b35e6cdb774002f825f81fad7a14133655a77be
Christopher <git@idragonmail.com>
2016-06-03 15:04:17 -0500

    Changed button to Save
    Changed the button to Save & Added the index

--- resources/views/admin/pages/index.blade.php
+++ resources/views/admin/pages/index.blade.php
@@ -1,22 +1,66 @@
 @extends('admin.layouts.master')
 
 @section('content')
-	<h1>Pages</h1>
+	<!-- Page Header -->
+	<section class="content-header">
+      <h1>
+        Pages
+        <small>Your single pages</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+        <li class="active">Pages</li>
+      </ol>
+    </section>
+    <!-- /.End Page Header -->
 
-	<ul>
-		@foreach($pages as $page)
-		<li>{{$page->id}}</li>
-		<li>{{$page->name}}</li>
-		<li>
-			<form action="{{ route('admin.pages.destroy', ['pages' => $page->id]) }}" method="POST">
-		        {{ csrf_field() }}
-		        {{ method_field('DELETE') }}
+    <section class="content">
+    	<div class="row">
+        	<div class="col-xs-12">
+			    <table id="example1" class="table table-bordered table-striped" cellspacing="0" width="100%">
+			        <thead>
+			            <tr>
+			            	<th>Title</th>
+			            	<th>Slug</th>
+			            	<th>Author</th>
+			            	<th>Date</th>
+			            	<th>Action</th>
+			            </tr>
+			        </thead>
+				    <tfoot>
+					    <tr>
+					    	<th>Title</th>
+					    	<th>Slug</th>
+			            	<th>Author</th>
+			            	<th>Date</th>
+			            	<th>Action</th>
+					    </tr>
+					</tfoot>
+			        <tbody>
+			        	@foreach($pages as $page)
+				        <tr>
+				            <td>{{$page->name}}</td>
+				            <td>{{$page->slug}}</td>
+				            <td>{{$page->user->name}}</td>
+				            <td>{{$page->updated_at->diffForHumans()}}</td>
+				            <td>
+				            	<a id="edit-page-{{ $page->id }}" href="{{route('admin.pages.edit', ['pages' => $page->id])}}" class="btn btn-link">
+							        Edit
+							    </a>
+				            	<form style="display:inline;" action="{{ route('admin.pages.destroy', ['pages' => $page->id]) }}" method="POST">
+							        {{ csrf_field() }}
+							        {{ method_field('DELETE') }}
 
-		        <button type="submit" id="delete-post-{{ $page->id }}" class="btn btn-danger">
-		            <i class="fa fa-btn fa-trash"></i>Delete
-		        </button>
-		    </form>
-		</li>
-		@endforeach
-	</ul>
+							        <button type="submit" id="delete-page-{{ $page->id }}" class="btn btn-link">
+							            Delete
+							        </button>
+							    </form>
+				            </td>
+				        </tr>
+				        @endforeach
+			        </tbody>
+				</table>
+			</div>
+		</div>
+    </section>
 @stop
