b0c25ea87dd6f9157bf6b9d7357ab888bf0466c6
Christopher <git@idragonmail.com>
2016-06-02 22:08:42 -0500

    Added Laravolt Avatar to composer

--- config/app.php
+++ config/app.php
@@ -156,6 +156,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Intervention\Image\ImageServiceProvider::class,
+        Laravolt\Avatar\ServiceProvider::class,
+
     ],
 
     /*
@@ -201,6 +204,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Avatar'    => Laravolt\Avatar\Facade::class,
 
     ],
 
