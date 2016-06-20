5880457ec813c23d0bbbc9f88e2cc5ff3a8c2716
Christopher <git@idragonmail.com>
2016-06-20 15:51:55 -0500

    Creating the codes table

---
+++ database/migrations/2016_06_20_203343_create_codes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('codes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->default(0);
+            $table->string('name')->nullable();
+            $table->text('content')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('user_id');
+            $table->index('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('codes');
+    }
+}
