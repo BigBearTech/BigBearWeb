aa1fcec4d0fc266d8df918f6a4bead54b0d64929
Christopher <git@idragonmail.com>
2016-06-20 15:17:39 -0500

    Added the config for bigbearweb
    Also added the admin_panel_url

---
+++ config/bigbearweb.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Admin Panel URL Prefix
+    |--------------------------------------------------------------------------
+    |
+    | If you change this your admin url will be what ever you type in.
+    | Be cautions in changing this setting if you don't know what your doing.
+    |
+    | @default admin
+    |
+    */
+    'admin_panel_url' => 'admin',
+];
